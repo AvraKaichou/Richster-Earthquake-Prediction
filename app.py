1efa3dbfef29e8a1901f95a2e309bf0d43e65dd8
AvraKaichou <146498936+AvraKaichou@users.noreply.github.com>
2025-05-26 09:18:43 +0700

    git delete old app.py

--- app.py
+++ app.py
@@ -7,8 +7,6 @@ Original file is located at
     https://colab.research.google.com/drive/1bnuDWFgZds_ybxdBgwYowIZwuuw9idYm
 """
 
-pip install streamlit joblib
-
 import streamlit as st
 import joblib
 import numpy as np
